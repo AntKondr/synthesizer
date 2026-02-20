1325787459cd09ee17fb07c3a4cb991a9c85af0c
Anton_Kondratev <elkondro@gmail.com>
2026-02-21 00:31:55 +0700

    refact: utils frange

--- synthesizer/utils.py
+++ synthesizer/utils.py
@@ -1,24 +1,77 @@
-from typing import Iterator
+from typing import Final, Iterable, Iterator, overload
 
 
-class FloatRange:
-    __slots__ = ()
+_STEP: Final[float] = 1.0
 
-    def __init__(self, start: float, stop: float, step: float, /) -> None:
-        if start >= stop:
-            raise ValueError(f"Stop ({stop}) is less or equal than start ({start})!")
-        self.start: float = start
+
+class _1Arg:
+    __slots__ = ("curr", "stop")
+
+    def __init__(self, stop: float, /) -> None:
+        self.curr: float = 0.0
         self.stop: float = stop
-        self.step: float = step
-        self.curr: float = start
 
     def __iter__(self) -> Iterator[float]:
         return self
 
     def __next__(self) -> float:
-        if self.curr >= self.stop:
-            self.curr = self.start
-            raise StopIteration
-        r: float = self.curr
-        self.curr += self.step
-        return r
+        if self.curr < self.stop:
+            r: float = self.curr
+            self.curr += _STEP
+            return r
+        raise StopIteration
+
+
+class _2Arg:
+    __slots__ = ("curr", "stop")
+
+    def __init__(self, start: float, stop: float, /) -> None:
+        self.curr: float = start
+        self.stop: float = stop
+
+    def __iter__(self) -> Iterator[float]:
+        return self
+
+    def __next__(self) -> float:
+        if self.curr < self.stop:
+            r: float = self.curr
+            self.curr += _STEP
+            return r
+        raise StopIteration
+
+
+class _3Arg:
+    __slots__ = ("curr", "stop", "step")
+
+    def __init__(self, start: float, stop: float, step: float, /) -> None:
+        self.curr: float = start
+        self.stop: float = stop
+        self.step: float = step
+
+    def __iter__(self) -> Iterator[float]:
+        return self
+
+    def __next__(self) -> float:
+        if self.curr < self.stop:
+            r: float = self.curr
+            self.curr += self.step
+            return r
+        raise StopIteration
+
+
+@overload
+def frange(stop: float, /) -> Iterable[float]: ...
+@overload
+def frange(start: float, stop: float, /) -> Iterable[float]: ...
+@overload
+def frange(start: float, stop: float, step: float, /) -> Iterable[float]: ...
+
+
+def frange(*a: float) -> Iterable[float]:
+    if (c := len(a)) == 1:
+        return _1Arg(*a)
+    if c == 2:
+        return _2Arg(*a)
+    if c == 3:
+        return _3Arg(*a)
+    raise TypeError
