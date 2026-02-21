662d32e47355b092083d24cf4419c32efa95e4de
Anton_Kondratev <elkondro@gmail.com>
2026-02-22 00:54:25 +0700

    mod: config

--- synthesizer/config.py
+++ synthesizer/config.py
@@ -4,9 +4,9 @@ from pathlib import Path
 from json import load
 
 
-# частота дискретизации (кол-во сэмплов в 1 секунде)
+# sampling rate (samples amount (length samples array) for 1 second)
 _DEFAULT_SAMPLE_RATE: Final[int] = 44100
-_SAMPLE_RATES: tuple[int, ...] = (
+_SAMPLE_RATES: Final[tuple[int, ...]] = (
     8000,           # 8000 Hz: telephony, voice calls (adequate for speech)
     16000,          # 16000 Hz: VoIP, video conferencing (better voice quality)
     11025, 22050,   # 11025 Hz / 22050 Hz: low-quality PCM, audio analysis
@@ -18,13 +18,16 @@ _SAMPLE_RATES: tuple[int, ...] = (
 
 # глубина, бит (signed)
 _DEFAULT_BIT_DEPTH: Final[int] = 16
-_BIT_DEPTHS: tuple[int, ...] = (8, 16, 24, 32)
+_BIT_DEPTHS: Final[tuple[int, ...]] = (8, 16, 24, 32)
+
+# buffer size for samples
+_DEFAULT_BUFF_SIZE: Final[int] = 512
 
 _DEFAULT_CONFIG_PATH: Final[Path] = Path(__file__).parent.parent / "config.json"
 
 
 class _Config:
-    __slots__ = ("_confFile", "sampleRate", "sampleRateF", "sampleTime", "bitDepth", "sampleWidth", "maxAmplitude", "minAmplitude")
+    __slots__ = ("_confFile", "sampleRate", "sampleRateF", "sampleTime", "bitDepth", "buffSize", "sampleWidth", "maxAmplitude", "minAmplitude")
 
     def __init__(self, confFile: Path) -> None:
         self._confFile: Final[Path] = confFile
@@ -33,24 +36,28 @@ class _Config:
         with open(self._confFile, "tr", -1, "utf_8") as f:
             conf: dict[str, Any] = load(f)
 
-        # частота дискретизации (семплирования)
-        # довольно фундаментальная штука
-        # при изменении скорее всего понадобится перезапускать приложение
+        # sampling rate
         if (v := conf.get("SAMPLE_RATE", _DEFAULT_SAMPLE_RATE)) not in _SAMPLE_RATES:
             raise ValueError(f"Invalid sample rate -> {v}")
         self.sampleRate: int = v
         self.sampleRateF: float = float(v)
+
+        # time between two samples
         self.sampleTime: float = 1.0 / self.sampleRateF
 
+        # bit depth
         if (v := conf.get("BIT_DEPTH", _DEFAULT_BIT_DEPTH)) not in _BIT_DEPTHS:
             raise ValueError(f"Invalid bit depth -> {v}")
         self.bitDepth: int = v
 
-        # ----- calculated fields ------------------------------
-        # сколько байт в глубине
+        # buffer size for samples
+        self.buffSize: int = int(conf.get("BUFF_SIZE", _DEFAULT_BUFF_SIZE))
+
+        # ----- calculated fields ------------------------------------
+        # bytes amount into depth
         self.sampleWidth: int = self.bitDepth // 8
 
-        # крайние значенния амплитуды для указанной глубины
+        # bound values ​​of amplitude for a given bit depth
         self.maxAmplitude: float = float((2 ** (self.bitDepth - 1)) - 1)
         self.minAmplitude: float = (self.maxAmplitude + 1.0) * -1.0
         return self
